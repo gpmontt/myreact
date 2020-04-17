c13a01a391e7686d5597444ef94fcef84dc71bee
gpmontt <30469479+gpmontt@users.noreply.github.com>
2020-04-17 17:20:20 +0200

    Updates

---
+++ precache-manifest.bf097170ae9f3f771f5402d948a98b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c5c72eb57ce6f8904b0fbbb0af1dee",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "49de0e86fa080457b30e",
+    "url": "/myreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3252f9a31a10765ce8bc",
+    "url": "/myreact/static/js/2.0847bb0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact/static/js/2.0847bb0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49de0e86fa080457b30e",
+    "url": "/myreact/static/js/main.1d521624.chunk.js"
+  },
+  {
+    "revision": "cec2abfeab37bc2f3b53",
+    "url": "/myreact/static/js/runtime-main.6391b570.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
